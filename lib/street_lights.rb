59684a86f01ca0123295f6fbab70f9cf3dbeec0a
kazuyoshi tlacaelel <kazu.dev@gmail.com>
2012-12-17 12:38:38 -0600

    Rack for Streetlights.

---
+++ lib/street_lights.rb
@@ -0,0 +1,26 @@
+require 'fileutils'
+require 'isna'
+
+class StreetLights
+
+  def initialize(app)
+    @app = app
+    @street_lights_dir = 'street_lights/'
+    unless File.exists?(@street_lights_dir)
+      FileUtils.mkdir_p(@street_lights_dir)
+    end
+  end
+
+  def call(env)
+    File.open("#{@street_lights_dir}/#{Process.pid}", 'w+') do |file|
+      file.puts "#{Process.pid.to_s.to_ansi.red.to_s} #{env['HTTP_HOST']} #{env['REQUEST_PATH']}"
+    end
+    status, headers, response = @app.call(env)
+    File.open("#{@street_lights_dir}/#{Process.pid}", 'w+') do |file|
+      file.puts "#{Process.pid.to_s.to_ansi.green.to_s}"
+    end
+    [status, headers, response]
+  end
+
+end
+
