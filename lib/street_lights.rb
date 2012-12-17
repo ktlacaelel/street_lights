69b78dc928975dc76637bf24258529762968ebf5
kazuyoshi tlacaelel <kazu.dev@gmail.com>
2012-12-17 15:27:23 -0600

    Handle diff exit methods while apps are restarting.

--- lib/street_lights.rb
+++ lib/street_lights.rb
@@ -9,15 +9,25 @@ class StreetLights
     unless File.exists?(@street_lights_dir)
       FileUtils.mkdir_p(@street_lights_dir)
     end
-    at_exit { File.rm "#{@street_lights_dir}/#{Process.pid}" }
+    shutdown_behaviour = proc do
+      FileUtils.rm(street_light) if File.exists?(street_light)
+    end
+    trap('QUIT', &shutdown_behaviour)
+    trap('TERM', &shutdown_behaviour)
+    trap('EXIT', &shutdown_behaviour)
+    trap('KILL', &shutdown_behaviour)
+  end
+
+  def street_light
+    @street_light ||= "#{@street_lights_dir}/#{Process.pid}"
   end
 
   def call(env)
-    File.open("#{@street_lights_dir}/#{Process.pid}", 'w+') do |file|
+    File.open(street_light, 'w+') do |file|
       file.puts "#{Process.pid.to_s.to_ansi.red.to_s} #{env['HTTP_HOST']} #{env['REQUEST_PATH']}"
     end
     status, headers, response = @app.call(env)
-    File.open("#{@street_lights_dir}/#{Process.pid}", 'w+') do |file|
+    File.open(street_light, 'w+') do |file|
       file.puts "#{Process.pid.to_s.to_ansi.green.to_s}"
     end
     [status, headers, response]
